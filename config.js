b35471955fd0b4cea8537fcfe772df4ed96c5844
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-18 23:22:38 +0100

    Export the checked environment

--- config.js
+++ config.js
@@ -24,3 +24,7 @@ var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.
 
 // Check that the current environment is one of the environments above, if not, default to staging
 var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+
+module.exports = environmentToExport;
